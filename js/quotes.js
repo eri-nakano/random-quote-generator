27df2faa081169f9b3f0919fec4c6838a7b3dfaa
Eri Nakano <33802747+eri-nakano@users.noreply.github.com>
2017-11-23 23:38:51 +0900

    Almost Final ver

---
+++ js/quotes.js
@@ -0,0 +1,39 @@
+// six quotes to be selected randomly
+var quotes = [
+  {
+    quote:"If you're going through hell, keep going.",
+    source:"Winston Churchill",
+    citation:"",
+    year:""
+  },
+  {
+    quote:"Whenever you find yourself on the side of the majority, it is time to pause and reflect.",
+    source:"Mark Twain",
+    citation:"",
+    year:"1904"
+  },
+  {
+    quote:"Fall seven times and stand up eight.",
+    source:"Japanese Proverb",
+    citation:"",
+    year:""
+  },
+  {
+    quote:"Tomorrow belongs to those who can hear it coming",
+    source:"David Bowie",
+    citation:"",
+    year:""
+  },
+  {
+    quote:"All you need is love. But a little chocolate now and then doesn't hurt.",
+    source:"Charles Schulz",
+    citation:"",
+    year:""
+  },
+  {
+    quote:"Love is a smoke and is made with the fume of sighs.",
+    source:"William Shakespeare",
+    citation:"",
+    year:""
+  }
+]
--- js/script.js
+++ js/script.js
@@ -1,34 +1,17 @@
-// six quotes to be selected randomly
-var quotes = [
-  ["If you're going through hell, keep going.", "Winston Churchill","",""],
-  ["Whenever you find yourself on the side of the majority, it is time to pause and reflect.", "Mark Twain","1904",""],
-  ["Fall seven times and stand up eight.", "Japanese Proverb","",""],
-  ["Tomorrow belongs to those who can hear it coming","David Bowie","",""],
-  ["All you need is love. But a little chocolate now and then doesn't hurt.","Charles Schulz","",""],
-  [ "Love is a smoke and is made with the fume of sighs.","William Shakespeare", "",""]
-];
-
-function getRandomQuotes(){
+var quote = '';
+var selectedQuote;
+//select one quote randomly from "quotes" array
+function getRandomQuote(){
 return quotes[Math.floor(Math.random() * quotes.length )];
 }
-function printQuote(){
-  var quoteBoxDiv = document.getElementById('quote-box').innerHTML;
-  quoteBoxDiv.innerHTML = <p class="quote"></p>
-                          <p class="source"></p>
-                          <span class="citation"> </span>
-                          <span class="year"> </span>
+//print the selected quote on a screen
+function printQuote(quote){
+  selectedQuote = getRandomQuote();
+    quote += '<p class="quote">' + selectedQuote.quote + '</p>';
+    quote += '<p class="source">' + selectedQuote.source + '</p>';
+    quote += '<span class="citation">' + selectedQuote.citation + '</span>';
+    quote += '<span class="year">' + selectedQuote.year + '</span>';
+  var quoteBoxDiv = document.getElementById('quote-box');
+  quoteBoxDiv.innerHTML = quote;
 }
-printQuote(getRandomQuotes());
-
-
-
-
-
-
-
-
-
-
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+printQuote(quote);
